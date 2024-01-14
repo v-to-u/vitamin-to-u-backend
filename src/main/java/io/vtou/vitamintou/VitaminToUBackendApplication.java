0c511004bdacff6251d3ecb14148576b7d3290a4
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-14 23:54:49 +0900

    INIT: Initial Commit

---
+++ src/main/java/io/vtou/vitamintou/VitaminToUBackendApplication.java
@@ -0,0 +1,13 @@
+package io.vtou.vitamintou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaminToUBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitaminToUBackendApplication.class, args);
+    }
+
+}
