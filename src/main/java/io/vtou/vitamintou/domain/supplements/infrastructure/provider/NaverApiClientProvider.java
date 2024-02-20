2ef64378739900dc01c1d42c6685c1a6d571b24b
sds <sds8154@tukorea.ac.kr>
2024-02-20 18:50:28 +0900

    naverApiProvider 수정

--- src/main/java/io/vtou/vitamintou/domain/supplements/infrastructure/provider/NaverApiClientProvider.java
+++ src/main/java/io/vtou/vitamintou/domain/supplements/infrastructure/provider/NaverApiClientProvider.java
@@ -2,15 +2,19 @@ package io.vtou.vitamintou.domain.supplements.infrastructure.provider;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Component
 @Getter
 public class NaverApiClientProvider {
-    private final String clientId;
-    private final String clientSecret;
-    public NaverApiClientProvider(@Value("${navershopping.client_id}") String cliendId,@Value("${navershopping.client_secret}") String clientSecret){
-        this.clientId=cliendId;
+    private String clientId="";
+    private String clientSecret="";
+    public NaverApiClientProvider(@Value("${navershopping.client_id}") String clientId, @Value("${navershopping.client_secret}") String clientSecret){
+        this.clientId=clientId;
         this.clientSecret=clientSecret;
     }
 }
